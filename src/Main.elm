eb9b272e837294d6853a9b1cc0a58d2be7a0facc
Sam Roberts <samgqroberts@gmail.com>
2021-03-11 14:29:24 -0500

    basic table with basic data

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,30 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img, table, td, text, th, thead, tr)
 import Html.Attributes exposing (src)
+
+
+
+---- DATA ----
+
+
+type alias DataType =
+    { name : String
+    , isMonoid : Bool
+    , isFunctor : Bool
+    , isApplicative : Bool
+    , isMonad : Bool
+    }
+
+
+data : List DataType
+data =
+    [ { name = "Maybe", isMonoid = True, isFunctor = True, isApplicative = True, isMonad = True }
+    , { name = "Either", isMonoid = True, isFunctor = True, isApplicative = True, isMonad = True }
+    , { name = "List", isMonoid = True, isFunctor = True, isApplicative = True, isMonad = True }
+    ]
+
 
 
 ---- MODEL ----
@@ -34,11 +56,43 @@ update msg model =
 ---- VIEW ----
 
 
+stringFromBool : Bool -> String
+stringFromBool x =
+    if x then
+        "True"
+
+    else
+        "False"
+
+
+dataRow : DataType -> Html Msg
+dataRow d =
+    tr []
+        [ td [] [ text d.name ]
+        , td [] [ text <| stringFromBool d.isMonoid ]
+        , td [] [ text <| stringFromBool d.isFunctor ]
+        , td [] [ text <| stringFromBool d.isApplicative ]
+        , td [] [ text <| stringFromBool d.isMonad ]
+        ]
+
+
+dataTable : List DataType -> Html Msg
+dataTable ds =
+    table [] <|
+        thead []
+            [ th [] [ text "Name" ]
+            , th [] [ text "Is Monoid" ]
+            , th [] [ text "Is Functor" ]
+            , th [] [ text "Is Applicative" ]
+            , th [] [ text "Is Monad" ]
+            ]
+            :: List.map dataRow ds
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ dataTable data
         ]
 
 
